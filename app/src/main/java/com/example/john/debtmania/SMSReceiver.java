27060dff82b5da207fac13f6a2aaf1b8450cb398
bfcadott <bfcadott@oakland.edu>
2014-11-17 15:43:58 -0500

    Created SMSReceiver

---
+++ app/src/main/java/com/example/john/debtmania/SMSReceiver.java
@@ -0,0 +1,33 @@
+package com.example.john.debtmania;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+public class SMSReceiver extends BroadcastReceiver {
+    static IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+    Main main = null;
+    SMSReceiver(Main _main)
+    {
+        main = _main;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+        final Object[] pdusObj = (Object[]) bundle.get("pdus");
+        SmsMessage message = SmsMessage.createFromPdu((byte[]) pdusObj[0]);
+        String msgString = message.getDisplayMessageBody();
+        String phoneNumber = message.getDisplayOriginatingAddress();
+
+        if(msgString.contains("OWE")) {
+            //add some info to database
+        }
+        else if(msgString.contains("COLLECTED")) {
+            //remove some info from the database
+        }
+    }
+}
